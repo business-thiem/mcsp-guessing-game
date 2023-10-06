6ecf4f9c113e2d076f64acba33b9bd1689e18345
Thiem Huynh <thiem.huynh0@gmail.com>
2023-10-06 14:47:34 -0400

    feature 5

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-// Feature 4
+ // Feature 5
+
+// function checkUser(){
+//     for (let i = 0; i < userArray.length; i++){
+//    if (userArray[i] === userName)
+// }
+
+
+let userArray = []
+let userName = prompt("What is your username?")
+userArray.push(userName);
 
 let isCorrect = false;
-
 let min = 1
 let max = 100
+
 function getBetween1and100 (min, max) {
     return Math.floor(Math.random() * (max - min) + min);
   }
@@ -17,22 +27,25 @@ let guessArray = []
 -1 make array take in strings and not just be NaN
 -2 for loop to iterate over array to check for prior guesses - see function checkRepeatedNums()
 */
+
+
+
 function checkResponse(){
     let userResponse = parseInt(prompt("Guess a number between 1 & 100."))
 
     if (number === userResponse){
-        alert(`Good job. Your guess of ${userResponse} was correct. You made ${guessArray.length} guesses.`)
+        alert(`Good job ${userName}. Your guess of ${userResponse} was correct. You made ${guessArray.length} guesses.`)
         alert(`Your previous guesses were ${guessArray}`)
         isCorrect = true;
     }
     else if (userResponse > number){
-        alert(`Your guess was ${userResponse}. Guess lower.`)
+        alert(`Sorry ${userName}. Your guess was ${userResponse}. Guess lower!`)
     }
     else if (userResponse < number){
-        alert(`Your guess was ${userResponse}. Guess higher.`)
+        alert(`Sorry ${userName}. Your guess was ${userResponse}. Guess higher!`)
     }
     else {
-        alert ('You are so wrong because your guess was not a number')
+        alert (`What is wrong with you ${userName}!!! You are so wrong because your guess was not a number`)
     } 
 
     guessArray.push(userResponse)
@@ -45,4 +58,4 @@ do{
     checkResponse();
 } while(!isCorrect)
 
-// console.log(guessArray)
+// console.log(guessArray) 
