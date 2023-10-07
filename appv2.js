738bfff9c15b356faea7940be8797df4cd9321de
Thiem Huynh <thiem.huynh0@gmail.com>
2023-10-06 20:46:22 -0400

    refactored feature 6, now working.

--- appv2.js
+++ appv2.js
@@ -1,45 +1,110 @@
 //working out feature 7
 
-let userArray, userName, isCorrect, min, max, number; 
+
+/* TODO 
+-print out prior guesses
+-check prior winner and their scores
+*/
+
+// let userArray, userName, isCorrect, min, max, number, newPlayer; 
 
 
-let game = {
+let gameInfo = {
     numberToGuess: 0,
     playerUserName: '',
     currentResponse: 0,
-    isSolved: false,
-    currentPlayerResponses: [],
-    pastScores: [] // ['thiem',3]
+    isNewPlayer: true,
+    responsesArr: [],
+    scoreBoard: ['tim', 3, 'bob', 5], // ['thiem',3]
+    attempts: 0
 }
 
-console.log(game);
+let min, max;
 
 
-startGame()
+// startGame(); now based on button click
 
 function startGame(){
-    setupGameBoard()
 
-    game.playerUserName = prompt('What is your name?');
-    game.currentResponse = prompt('Guess a number between 1 and 3')
-    
-    console.log(game)
-    // add validateUserResponse()
+    setupGameBoard();
+    let isSolved = false;
+
+
+    gameInfo.playerUserName = prompt('What is your name?');
+    while(!isSolved){
+          
+        if(gameInfo.attempts > 0){
+            isSolved = playGame();
+            gameInfo.attempts--;
+        }else {
+            alert('you lost the game')
+            isSolved = true;
+        }
+    }
+
+    console.log('isSolved value:', isSolved)
+    console.log('end gameInfo',gameInfo)
 }
+
+function playGame(){
+    let promptMessage = 
+    `Guess a number between ${min} and ${max} \n
+    You have ${gameInfo.attempts} attempts left`
+
+    
+    gameInfo.currentResponse = Number(prompt(promptMessage))
+    gameInfo.responsesArr.push(gameInfo.currentResponse)
+
+    return isSolved = validateUserResponse(gameInfo)
+
+}
+
+function priorPlayerCheck(){
+    // run some check with array.includes, but for now return true
+    return true;
+}
+
+
+
+
+function validateUserResponse(gameObj){
+    let num = gameObj.numberToGuess
+    let playerGuess = gameObj.currentResponse
+    let isCorrect = gameObj.isSolved
+    let guesses = gameInfo.responsesArr
+
+    let strVictory = `Congrats you\'re correct! Your guess was: ${playerGuess}
+    \n The number to guess was: ${num}
+    \n Here were all your guesses ${guesses.join(', ')}`
+    
+    if(num === playerGuess){
+        alert(strVictory)
+        isCorrect = true
+    } else if(num > playerGuess){
+        alert(`Guess higher. try again: hint ${num}`)
+    } else if(num < playerGuess){
+        alert(`Guess lower. try again: hint ${num}`)
+    }
+
+
+    return isCorrect;
+}
+
 
 function setupGameBoard(){
     min = 1
-    max = 3
+    max = 100
 
-    game.numberToGuess = getNumberToGuess(min, max)
+    gameInfo.numberToGuess = getNumberToGuess(min, max)
+    gameInfo.isSolved = false;
+    gameInfo.currentPlayerResponses = []
+    gameInfo.attempts = 5
 
-    game.isSolved = false;
-    game.currentPlayerResponses = []
-
+    console.log('start gameInfo:', gameInfo);
 }
 
 function getNumberToGuess(min, max) {
-    return Math.ceil(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min) + min + 1);
 }
 
 
---
+++ includesChecker.js
@@ -0,0 +1,8 @@
+let 
+
+
+function priorPlayerCheck(){
+    // run some check with array.includes, but for now return true
+    forEach()
+    return true;
+}
