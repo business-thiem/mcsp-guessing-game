763fdd9c50fe1692e4f7f39657eb849df947088a
Thiem Huynh <thiem.huynh0@gmail.com>
2023-10-10 14:36:28 -0400

    finished refactor and implementing garret logic

--- appv3/appv3.js
+++ appv3/appv3.js
@@ -32,9 +32,10 @@ function startGame() {
 
   //show message player won
   displayResultMessage(playerName);
-  //record the score
-
+  //record the score and reset
+  resetscoreBoard(playerName);
   //ask to play again
+  playAgain();
 }
 
 function askForNumber() {
@@ -65,28 +66,31 @@ function displayResultMessage(playerName) {
 }
 
 function compareWithPreviousGame(playerName) {
-  const priorScore = scoreBoard[playerName].past.length; //2
-  const currentScore = scoreBoard[playerName].current.length; //5
-  const scoreDifference = currentScore - priorScore; // 5-2 did not do better
+  const priorScore = scoreBoard[playerName].past.length;
+  const currentScore = scoreBoard[playerName].current.length;
+  const scoreDifference = currentScore - priorScore;
 
-  // 3 diff did not do better
+  // diff did not do better
   if (scoreDifference < 0) {
     alert(
-      `That's Correct ${guest}! You missed your highscore by ${Math.abs(
+      `That's Correct ${playerName}! You missed your highscore by ${Math.abs(
         difference
       )} guesses.`
     );
     //if diff did better
   } else if (difference > 0) {
     alert(
-      `That's Correct ${guest}! You did better in your last game by ${difference} guesses.`
+      `That's Correct ${playerName}! You did better in your last game by ${difference} guesses.`
     );
   }
-}
-
-function resetscoreBoard(guest) {
-  scoreBoard[guest].past = [...scoreBoard[guest].current]; //spread operator, current scoreBoard set are now past scoreBoard
-  scoreBoard[guest].current = [];
+  //if diff is 0 (equal)
+  else {
+    alert(
+      `That's Correct ${playerName}! You had the same number of guesses in your last game. This round you had these guesses: ${scoreBoard[
+        playerName
+      ].current.join(', ')}.`
+    );
+  }
 }
 
 function getNumberToGuess(min, max) {
@@ -99,6 +103,29 @@ function initializeUser(userName) {
       current: [],
       past: [],
     };
+  }
+}
+
+function resetscoreBoard(playerName) {
+  scoreBoard[playerName].past = [...scoreBoard[playerName].current]; //spread operator, current scoreBoard set are now past scoreBoard
+  scoreBoard[playerName].current = [];
+}
+
+function playAgain() {
+  //TODO add input validation
+  const response = prompt('Do you want to play again? Yes / No');
+
+  switch (response) {
+    case 'Yes':
+      alert('Ok started a new game!');
+      play();
+      break;
+    case 'No':
+      alert("Cool, I'm sure you have better things to do");
+      break;
+    default:
+      alert("Sorry I didn't recognize that command");
+      playAgain();
   }
 }
 
